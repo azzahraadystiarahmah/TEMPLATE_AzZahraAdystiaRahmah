baddf8bd4d9bf461549a98042f2e5a0397c0fb2e
azzahraadystiarahmah <91015436+azzahraadystiarahmah@users.noreply.github.com>
2021-10-10 23:54:18 +0700

    Update about.php

--- application/views/about.php
+++ application/views/about.php
@@ -1,5 +1,5 @@
 <main role="main" class="container">
-      <h1 class="mt-5">Profil Page</h1>
+      <h1 class="mt-5">About Page</h1>
       <p class="lead">Selamat Datang Pada Halaman Profil</p>
       <p class="lead">About berisi tentang kumpulan gallery saya</p>
     </main>
