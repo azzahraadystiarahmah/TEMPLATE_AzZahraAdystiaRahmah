e107f31bb6c6d581ea6f5946a473774c82d63feb
azzahraadystiarahmah <azzahraadystiarahmah@gmail.com>
2021-10-10 23:18:02 +0700

    Tugas Mandiri Web Programming II Pertemuan 4

---
+++ application/views/about.php
@@ -0,0 +1,5 @@
+<main role="main" class="container">
+      <h1 class="mt-5">Profil Page</h1>
+      <p class="lead">Selamat Datang Pada Halaman Profil</p>
+      <p class="lead">About berisi tentang kumpulan gallery saya</p>
+    </main>
